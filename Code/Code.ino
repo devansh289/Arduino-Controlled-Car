f7ad8f0aec6b62ac5c413f02deb27e05f2e67527
Devansh Sharma <devanshsharma289@gmail.com>
2018-01-14 21:05:13 -0500

    Supports Bluetooth

--- Code/Code.ino
+++ Code/Code.ino
@@ -2,6 +2,7 @@ const int motor1 = 5;         //motor1 and motor2 --> First motor
 const int motor2 = 6;
 const int motor3 = 8;        //motor3 and motor4 --> Second motor
 const int motor4 = 9;
+char command;
  
 void setup()
 {
@@ -9,58 +10,76 @@ void setup()
     pinMode(motor2, OUTPUT);
     pinMode(motor3, OUTPUT);
     pinMode(motor4, OUTPUT);
+
+    Serial.begin(9600);
 }
- 
- 
-void loop()
-{
-    digitalWrite(motor1, LOW);      //Forward
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3 , LOW);
-    digitalWrite(motor4 , HIGH);
-    delay(1000);
-   
-    digitalWrite(motor1, HIGH);      //Pause
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-    
-    
-    digitalWrite(motor1, HIGH);     //Backwards
-    digitalWrite(motor2, LOW);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, LOW);
-    delay(1000);
-   
-    digitalWrite(motor1, HIGH);      //Pause
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-   
-    digitalWrite(motor1, HIGH);      //Left
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, LOW);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-   
-    digitalWrite(motor1, HIGH);      //Pause
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-   
-    digitalWrite(motor1, LOW);      //Right
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-    
-    digitalWrite(motor1, HIGH);      //Pause
-    digitalWrite(motor2, HIGH);
-    digitalWrite(motor3, HIGH);
-    digitalWrite(motor4, HIGH);
-    delay(1000);
-    
+
+
+void loop() {
+  if(Serial.available() > 0) {
+    command = Serial.read();
+    breaks();
+    switch(command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        reverse();
+        break;
+      case 'R':
+        right();
+        break;
+      case 'L':
+        left();
+        break;
+    }
+  }
 }
+
+
+void forward() {
+  digitalWrite(motor1, LOW);      //Forward
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3 , LOW);
+  digitalWrite(motor4 , HIGH);
+  delay(1000);
+  Serial.println("Forward Motion");
+}
+
+void reverse() {
+  digitalWrite(motor1, HIGH);     //Backwards
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, LOW);
+  delay(1000);
+  Serial.println("Reverse Motion");
+}
+
+void right() {
+  digitalWrite(motor1, LOW);      //Right
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, HIGH);
+  delay(1000);
+  Serial.println("Right Motion: Left Motor Forward");
+}
+
+void left() {
+  digitalWrite(motor1, HIGH);      //Left
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, HIGH);
+  delay(1000);
+  Serial.println("Left Motion: Right Motor Forward");
+}
+
+
+void breaks() {
+  digitalWrite(motor1, HIGH);      //Pause
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, HIGH);
+  delay(1000);
+  Serial.println("Breaks");
+}
+
