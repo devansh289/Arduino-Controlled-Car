4b443b0a96ec1b3337d808ce4164acfa2c73e2a3
Devansh Sharma <devanshsharma289@gmail.com>
2018-01-16 17:24:36 -0500

    Fixed Formatting

--- Code/Code.ino
+++ Code/Code.ino
@@ -3,19 +3,19 @@ const int motor2 = 6;
 const int motor3 = 8;        //motor3 and motor4 --> Second motor
 const int motor4 = 9;
 char command;
- 
+
 void setup()
 {
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
   pinMode(motor3, OUTPUT);
   pinMode(motor4, OUTPUT);
- 
+
   Serial.begin(9600);
- 
+
 }
- 
- 
+
+
 void loop() {
   // Add while if problems
   if (Serial.available() > 0) {
@@ -37,8 +37,8 @@ void loop() {
     }
   }
 }
- 
- 
+
+
 void forward() {
   digitalWrite(motor1, LOW);      //Forward
   digitalWrite(motor2, HIGH);
@@ -47,7 +47,7 @@ void forward() {
   delay(1000);
   Serial.println("Forward Motion");
 }
- 
+
 void reverse() {
   digitalWrite(motor1, HIGH);     //Backwards
   digitalWrite(motor2, LOW);
@@ -56,7 +56,7 @@ void reverse() {
   delay(1000);
   Serial.println("Reverse Motion");
 }
- 
+
 void left() {
   digitalWrite(motor1, HIGH);      //Right
   digitalWrite(motor2, HIGH);
@@ -65,7 +65,7 @@ void left() {
   delay(1000);
   Serial.println("Right Motion");
 }
- 
+
 void right() {
   digitalWrite(motor1, LOW);      //Left
   digitalWrite(motor2, HIGH);
@@ -74,8 +74,8 @@ void right() {
   delay(1000);
   Serial.println("Left Motion");
 }
- 
- 
+
+
 void breaks() {
   digitalWrite(motor1, HIGH);      //Pause
   digitalWrite(motor2, HIGH);
